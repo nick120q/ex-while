cde297dd981fd02d078750067619bf683ee8c31f
nick120q <nicollas.s.bar@gmail.com>
2026-04-14 20:57:03 -0300

    feat(ex06):media notas

--- src/atividades/mediaNotas.java
+++ src/atividades/mediaNotas.java
@@ -2,18 +2,20 @@ package atividades;
 import java.util.Scanner;
 public class mediaNotas {
     static void main() {
-        int nota, divisao, media;
+        int nota, divisao, media, nota1;
         Scanner sc = new Scanner(System.in);
         IO.println("Digite suas notas e calcule a média(digite -1 para parar)");
         divisao = 0;
-        nota = 0;
+        nota1 = 0;
         while(true){
+            nota = sc.nextInt();
             if (nota == -1){
                 break;
             }
+            nota1 += nota;
             divisao++;
         }
-        media = nota / divisao;
+        media = nota1 / divisao;
         IO.print("Sua média é: " + media);
     }
 }
